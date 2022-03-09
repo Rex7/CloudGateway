80e8ea6c5fb14b6c5a87ab922756c6f0202346bd
rex7 <regischarles7@gmail.com>
2022-03-10 00:30:59 +0530

    did some changes for supporting cloud config server

--- src/main/java/com/example/demo/controller/FallBackController.java
+++ src/main/java/com/example/demo/controller/FallBackController.java
@@ -1,13 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
+@RefreshScope
 public class FallBackController {
 
+ 
+	
 	@GetMapping("/employeeFallback")	
 	public String employeeServicellFallback() {
 		System.out.print("employeeCalled");
@@ -16,8 +21,8 @@ public class FallBackController {
 	}
 
 	@GetMapping("/departmentFallback")
-	//@CircuitBreaker(name="CircuitBreaker")
 	public String departmentServiceFallback() {
+		System.out.print("departmentcalled");
 		return "fallback to department service due to service unavailbility";
 	}
 	
